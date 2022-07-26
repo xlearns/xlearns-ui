810310fc417a5817f962bb176459f90c77673734
root <root@LAPTOP-4JFFFIEH.localdomain>
2022-07-26 14:57:59 +0800

    fix

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -6,4 +6,7 @@ import DefineOptions from "unplugin-vue-define-options/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), DefineOptions()],
+	server:{
+		host:true
+	}
 });
