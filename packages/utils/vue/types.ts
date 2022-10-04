c60b607c40c801ca57fbc5e69dfb3b17eec2307a
xlearns <2447684095@qq.com>
2022-10-05 01:47:07 +0800

    fix: preserveSymlinks on build for ts error

--- packages/utils/vue/types.ts
+++ packages/utils/vue/types.ts
@@ -1,7 +1,7 @@
-import type { AppContext } from "vue";
+import type { AppContext, Plugin } from "vue";
 
-export type SFCWithInstall<T> = T & any; // T & Plugin gen dts error ts2742
+export type SFCWithInstall<T> = T & Plugin; // T & Plugin gen dts error ts2742
 
 export type SFCInstallWithContext<T> = SFCWithInstall<T> & {
-	_context: AppContext | null;
+  _context: AppContext | null;
 };
