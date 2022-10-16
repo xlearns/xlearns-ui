aa66c2f2579a807e0850666c9d7d894c543c64ea
xlearns <2447684095@qq.com>
2022-10-16 23:51:07 +0800

    chore: add grid-layout add props css

--- packages/components/grid-layout/src/grid-layout.vue
+++ packages/components/grid-layout/src/grid-layout.vue
@@ -1,15 +1,50 @@
 <script setup lang="ts">
-import { gridLayoutProps } from "./grid-layout";
+import { computed } from "vue";
 import { useNamespace } from "@element3/hooks";
 defineOptions({
   name: "ElGridLayout",
 });
-defineProps({ ...gridLayoutProps });
+
+const props = defineProps({
+  columns: {
+    type: String,
+    default: "1fr",
+  },
+  rows: {
+    type: String,
+    default: "1fr",
+  },
+  areas: {
+    type: Array,
+    default: [],
+  },
+  gap: {
+    type: String,
+    default: "",
+  },
+  height: {
+    type: String,
+    default: "100%",
+  },
+});
+
 defineEmits({});
+
 const ns = useNamespace("grid-layout");
+
+const style = computed(() => {
+  const areas = `nav nav nav nav`;
+  return {
+    "grid-template-columns": props.columns,
+    "grid-template-rows": props.rows,
+    "grid-template-areas": areas,
+    "grid-gap": props.gap,
+    height: props.height,
+  };
+});
 </script>
 
 <template>
-  <div :class="ns.b()"><slot /></div>
+  <div :class="ns.b()" :style="style"><slot /></div>
 </template>
 <style scoped></style>
